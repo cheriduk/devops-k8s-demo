d1378fc0343e40387f3f6166e446805d8182fdea
dukang <15162678717@163.com>
2020-12-06 23:09:56 +0800

    ss

---
+++ src/main/java/com/gary/devopsk8sdemo/DevopsK8sDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gary.devopsk8sdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsK8sDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsK8sDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gary/devopsk8sdemo/DevopsK8sDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gary.devopsk8sdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsK8sDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

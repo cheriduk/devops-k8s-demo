00361aa3620cbe66f3c64a9b4def48a29acf84f9
dukang <15162678717@163.com>
2020-12-06 23:22:33 +0800

    hello

--- src/main/java/com/gary/devopsk8sdemo/DevopsK8sDemoApplication.java
+++ src/main/java/com/gary/devopsk8sdemo/DevopsK8sDemoApplication.java
@@ -2,12 +2,20 @@ package com.gary.devopsk8sdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DevopsK8sDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevopsK8sDemoApplication.class, args);
+    }
+
+    @GetMapping("say")
+    public String hello(){
+        return "hello k8s devops";
     }
 
 }

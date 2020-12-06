feae8214eeae724bfff8abc8d4ec9bc3a493d0bd
dukang <15162678717@163.com>
2020-12-07 03:31:34 +0800

    hello-docker-v5

--- src/main/java/com/gary/devopsk8sdemo/DevopsK8sDemoApplication.java
+++ src/main/java/com/gary/devopsk8sdemo/DevopsK8sDemoApplication.java
@@ -15,7 +15,7 @@ public class DevopsK8sDemoApplication {
 
     @GetMapping("/say")
     public String hello(){
-        return "hello k8s devops-v4";
+        return "hello k8s devops-v5";
     }
 
 }
